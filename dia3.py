d15293d61740cfdab4ca1b55a0659f9dd2af2843
jr-renato <jr.renato@gmail.com>
2026-01-19 18:43:06 -0300

    Add files via upload
    entrada de dados e conversão com int

---
+++ dia3.py
@@ -0,0 +1,7 @@
+nome = input("Digite seu nome: ")
+print("Bem vindo,", nome)
+ano_atual = int(input("Digite o ano atual: "))
+ano_nascimento = int(input("Digite seu ano de nascimento: "))
+idade = ano_atual - ano_nascimento
+print("Você tem", idade, "anos")
+altura = float(input("Digite sua altura (Exemplo: 1.85): "))
