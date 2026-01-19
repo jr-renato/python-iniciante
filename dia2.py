219d6d905091224beed974a1d4e21dfe2ad99c7c
jr-renato <jr.renato@gmail.com>
2026-01-19 15:49:02 -0300

    Add files via upload
    Dia 2 - Variáveis e tipos de dados

---
+++ dia2.py
@@ -0,0 +1,10 @@
+ano_atual = 2026
+ano_nascimento = 1984
+nome = "Renato"
+idade = ano_atual - ano_nascimento
+altura = 1.70
+estudando_phyton = True
+print("Meu nome é", nome)
+print("Tenho", idade, "anos")
+print("Minha altura é", altura)
+print(estudando_phyton)
