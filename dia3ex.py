5b408590c9559c4ff2906e9a3d35be1655b28599
jr-renato <jr.renato@gmail.com>
2026-01-20 09:46:11 -0300

    Add files via upload
    Exercicios extra dia 3

---
+++ dia3ex.py
@@ -0,0 +1,20 @@
+idade = int(input("Digite sua idade (Apenas números) : "))
+if idade >= 18:
+    print("Você é maior de idade")
+else:
+    print("Você é menor de idade")
+Nome = input("Digite seu nome (Apenas letras) : ")
+if Nome == "Renato":
+    print("Nome reconhecido")
+else:
+    print("Nome desconhecido")
+senha = input("Digite sua senha: ")
+if senha == "12345":
+    print("Senha correta")
+else:
+    print("Senha incorreta")
+nota = float(input("Digite sua nota: "))
+if nota >= 7:
+    print("Aprovado")
+else:
+    print("Reprovado")
